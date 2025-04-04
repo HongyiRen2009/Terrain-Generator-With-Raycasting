a1b32c54efffb0e937f2b29f0f9bb0f2887a4a3f
Nuaym Syed <scienceboy75@gmail.com>
2025-04-03 22:32:59 -0700

    Refactor: More wrapper classes!

--- src/Map.ts
+++ src/Map.ts
@@ -1,5 +1,7 @@
 //Wrapper classes (will write stuff later)
+
 //Check README for implementation pattern
+//Center chunk starts at 0,0 (probably)
 
 //Entirety of the map
 class Map{
@@ -7,6 +9,7 @@ class Map{
     //In Chunks
     private width: number;
     private length: number;
+    
     private chunks: Chunk[];
 
     //TODO: Insert parameters
@@ -15,16 +18,45 @@ class Map{
         this.length = length;
         this.height = height;
         this.chunks = [];
+        this.generate();
+    }
+
+    //Generates map
+    public generate(){
+
+    }
+
+    //Renders map (later implementation we don't care abt it rn.)
+    public render(){
+    
     }
 
 }
 
 // Each individual chunk
 class Chunk{
+    // # "cubes"
     private width: number = 32;
     private length: number = 32;
     private height: number;
+
+    private matrice: Point[];
     public constructor(height: number){
         this.height = height;
+        this.matrice = [];
+    }
+}
+
+class Point{
+    //TODO: Write thingies
+    private elevation: number;
+    private x: number;
+    private y: number;
+    private type: string; //String for now. Probably should switch to a class later on. Will be stuff like "ground, water, etc."
+    public constructor(x: number, y: number, elevation: number, type: string){
+        this.x = x;
+        this.y = y;
+        this.elevation = elevation;
+        this.type = type;
     }
 }
