2cba6775939e416c44a4a086de26235286289947
Lucas <lucaswulu@hotmail.com>
2025-05-19 19:51:48 -0700

    fix: github syncing problems

--- src/map/Map.ts
+++ src/map/Map.ts
@@ -33,22 +33,22 @@ export class WorldMap {
     this.chunks = [
       new Chunk(
         vec2.fromValues(0, 0),
-        vec3.fromValues(32, 32, 32),
+        vec3.fromValues(this.resolution, this.resolution, this.resolution),
         this.simplexNoise
       ),
       new Chunk(
-        vec2.fromValues(32, 0),
-        vec3.fromValues(32, 32, 32),
+        vec2.fromValues(this.resolution, 0),
+        vec3.fromValues(this.resolution, this.resolution, this.resolution),
         this.simplexNoise
       ),
       new Chunk(
-        vec2.fromValues(64, 0),
-        vec3.fromValues(32, 32, 32),
+        vec2.fromValues(2*this.resolution, 0),
+        vec3.fromValues(this.resolution, this.resolution, this.resolution),
         this.simplexNoise
       ),
       new Chunk(
-        vec2.fromValues(0, 32),
-        vec3.fromValues(32, 32, 32),
+        vec2.fromValues(0, this.resolution),
+        vec3.fromValues(this.resolution, this.resolution, this.resolution),
         this.simplexNoise
       )
     ];
