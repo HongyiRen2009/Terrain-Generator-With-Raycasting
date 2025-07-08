f79bc013227661980169943b22b5d9570c699a3d
Nuaym Syed <scienceboy75@gmail.com>
2025-07-08 15:55:38 -0700

    misc: Cleanup

--- src/map/terrains.ts
+++ src/map/terrains.ts
@@ -45,14 +45,14 @@ export interface Terrain {
   type: 1 | 2 | 3 | 4 | 5; //look below
   /* Note about type (if no like search it up)
   Here is a list of types of surfaces (from chatgpt)
-  1. Diffuse (Labertian)
+  1. Diffuse (Labertian) ; Regular matte surface
   2. Specular (Perfect Mirror)
     //Color doesn't matter
   3. Glossy (Microfacet Reflection)
     //Note: roughness 0 = perfect mirror, roughness 1 = diffuse
   4. Transmission (Dielectric/Glass; Tinted Glass)
     //Note: roughness refers to index of refraction
-  5. Emission
+  5. Emission; Emissive surface (not as strong as regular light)
 
 
   //Future: consider Anisotropic surfaces (like brushed metal); Basically needs full textures
@@ -65,31 +65,31 @@ export interface Terrain {
  * The class for calculating the information for all our terrain types
  */
 export const Terrains: { [id: number]: Terrain } = { //NOTE: WHEN ADD TERRAINS CHANGE numberTerrains in packTerrainTypes in BVHUtils.ts and NUM_TERRAINS in glslPath.ts
-  0: {
+  0: { //Regular ground
     color: new Color(0, 255, 0),
     reflectiveness: 0.2,
     roughness: 0.8,
     type: 1
   },
-  1: {
+  1: { // Perfect mirror
     color: new Color(0, 0, 255),
     reflectiveness: 0.2,
     roughness: 0.8,
     type: 2
   },
-  2: {
+  2: { // Glossy surface
     color: new Color(255, 0, 0),
     reflectiveness: 0.2,
     roughness: 0.0,
     type: 3
   },
-  3: {
+  3: { // Tinted glass
     color: new Color(100, 0, 0), //red tint
     reflectiveness: 0.2,
     roughness: 1.5, //Index of refraction (look up)
     type: 4
   },
-  4: {
+  4: { // Emissive surface
     color: new Color(255, 0, 0),
     reflectiveness: 0.2,
     roughness: 0.8,
