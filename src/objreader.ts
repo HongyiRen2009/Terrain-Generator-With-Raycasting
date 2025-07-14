209b2244f0502874c6babb823705828388ad301d
Bulmenisaurus <63682890+Bulmenisaurus@users.noreply.github.com>
2025-07-14 12:15:28 -0700

    feat: proper normals for meshes read from .obj

--- src/objreader.ts
+++ src/objreader.ts
@@ -1,6 +1,15 @@
 import { vec3 } from "gl-matrix";
 import { Mesh } from "./map/Mesh";
 
+const calculateNormal = (vertices: vec3[]): vec3 => {
+  const normal = vec3.create();
+  const v1 = vec3.subtract(vec3.create(), vertices[1], vertices[0]);
+  const v2 = vec3.subtract(vec3.create(), vertices[2], vertices[0]);
+  vec3.cross(normal, v1, v2);
+  vec3.normalize(normal, normal);
+  return normal;
+};
+
 //TODO: .obj comes with built in optimization reusing vertices
 export const objSourceToMesh = (objSource: string): Mesh => {
   let i = 0;
@@ -18,14 +27,13 @@ export const objSourceToMesh = (objSource: string): Mesh => {
     } else if (line.startsWith("f")) {
       // note: vertix indices start at 1
       let [_, t1, t2, t3] = line.split(" ");
-      mesh.addTriangle(
-        [
-          vertices[parseInt(t1) - 1],
-          vertices[parseInt(t2) - 1],
-          vertices[parseInt(t3) - 1]
-        ],
-        [vec3.create(), vec3.create(), vec3.create()]
-      );
+      let triangle: [vec3, vec3, vec3] = [
+        vertices[parseInt(t1) - 1],
+        vertices[parseInt(t2) - 1],
+        vertices[parseInt(t3) - 1]
+      ];
+      let normal = calculateNormal(triangle);
+      mesh.addTriangle(triangle, [normal, normal, normal]);
     } else {
       throw new Error(
         `Unexpected line start ${line[0]} of ${line} at line ${i}`
